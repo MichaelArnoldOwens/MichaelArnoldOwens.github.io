4494bb97bf5f5d15e14a9c515638631affed5a04
Michael Owens <mowens@ucdavis.edu>
2020-04-20 03:02:43 -0700

    Updates

---
+++ precache-manifest.81ed51408705f20b07f6e8fda0ba3719.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79562e4a504b9c9b75d649aa655f9b17",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "dbf742466808b332ce15",
+    "url": "/home/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9a044066c621109156a2",
+    "url": "/home/static/js/2.86d6ef18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/home/static/js/2.86d6ef18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbf742466808b332ce15",
+    "url": "/home/static/js/main.a6f890ec.chunk.js"
+  },
+  {
+    "revision": "28c5e965a57e06b6af03",
+    "url": "/home/static/js/runtime-main.f2fdb678.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  }
+]);
